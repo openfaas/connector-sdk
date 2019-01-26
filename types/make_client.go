32f6d1b5558635cee614bb987b2604b1cb78b1b4
Michael Gasch <embano1@live.com>
2019-01-26 20:22:05 +0100

    Add timeout for http.Client
    - Invoker http.Client now also uses the user-defined upstream
    timeout for http requests (before: only net.Dialer used the timeout for
    initial connection establishing)
    
    Signed-off-by: Michael Gasch <embano1@live.com>

--- types/make_client.go
+++ types/make_client.go
@@ -6,11 +6,15 @@ import (
 	"time"
 )
 
+// MakeClient returns a http.Client with a timeout for connection establishing and request handling
 func MakeClient(timeout time.Duration) *http.Client {
 	return &http.Client{
 		Transport: &http.Transport{
 			Proxy: http.ProxyFromEnvironment,
 			DialContext: (&net.Dialer{
+				// Timeout is the maximum amount of time a dial will wait for
+				// a connect to complete. If Deadline is also set, it may fail
+				// earlier.
 				Timeout:   timeout,
 				KeepAlive: 10 * time.Second,
 			}).DialContext,
@@ -18,5 +22,11 @@ func MakeClient(timeout time.Duration) *http.Client {
 			MaxIdleConnsPerHost: 100,
 			IdleConnTimeout:     120 * time.Millisecond,
 		},
+		// Timeout specifies a time limit for requests made by this
+		// Client. The timeout includes connection time, any
+		// redirects, and reading the response body. The timer remains
+		// running after Get, Head, Post, or Do return and will
+		// interrupt reading of the Response.Body.
+		Timeout: timeout,
 	}
 }
